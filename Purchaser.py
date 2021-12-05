1a86797ecbcc5813129d05095d10b99ced3349f0
CeNeCheat <dedie.mike111@gmail.com>
2021-12-05 14:36:25 +0300

    Инит

--- Purchaser.py
+++ Purchaser.py
@@ -1,4 +1,3 @@
-customer = []
 class Purchaser:
     def __init__(self, name=None, surname=None, creditcard=None):
         self.__name = name
@@ -9,11 +8,6 @@ class Purchaser:
         print("Номер карты: ", self.__creditcard)
 pur1 = Purchaser(name=input("Введите имя: "), surname=input("Введите фамилию"), creditcard=input("Введите номер карты: "))
 pur2 = Purchaser(name="Григорий", surname="Анатолий", creditcard="0000")
-purlist = []
-for count in range(2):
-    x= pur1()
-    x.attr = count
-    purlist.append(x)
 pur1.printInfo()
 pur2.printInfo()
     
